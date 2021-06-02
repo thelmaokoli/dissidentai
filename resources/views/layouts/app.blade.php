99e819089e65f4f9b6f624e99bcb01339faa97ea
thelmaijemma <okoli.thelma@gmail.com>
2021-06-02 20:19:09 +0100

    added css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,14 +11,14 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
-<script src="{{ asset('ckeditor/ckeditor.js') }}"></script>
+<script src="{{ asset('//www.dissidentai.com/ckeditor/ckeditor.js') }}"></script>
 <script> CKEDITOR.replace( 'summary-ckeditor' );</script>
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('//www.dissidentai.com/css/app.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 </head>
 <div>
